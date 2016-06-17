01f66f42ad3fc47041d11a594ec9019c4a4dc534
Tomáš Kukrál <kukratom@fit.cvut.cz>
2016-06-17 09:06:01 +0200

    add error handling

--- templates/afacts.py
+++ templates/afacts.py
@@ -17,41 +17,54 @@ smart_params = [
 data = {
     'disks': {},
     'ipmi': {},
-    'timestamp': datetime.datetime.utcnow().isoformat()
+    'timestamp': datetime.datetime.utcnow().isoformat(),
+    'errors': {}
 }
 
 # disks
-fdisk_proc = subprocess.Popen(['fdisk', '-l'], stdout=subprocess.PIPE)
-for fdisk_line in fdisk_proc.stdout:
-    fdisk_line = fdisk_line.rstrip()
-    fdisk_sek = re.search('^Disk /dev/(sd.*?): (.*?),', fdisk_line)
-    if fdisk_sek:
-        # basic information
-        disk = {}
-        disk['name'] = fdisk_sek.group(1)
-        disk['capacity'] = fdisk_sek.group(2)
+try:
+    fdisk_proc = subprocess.Popen(['fdisk', '-l'], stdout=subprocess.PIPE)
+except OSError as e:
+    fdisk_proc = False
+    data['errors']['disks'] = str(e)
 
-        # SMART
-        smart_proc = subprocess.Popen(['smartctl', '-a', '/dev/' + disk['name']], stdout=subprocess.PIPE)
-        disk['smart'] = {}
+if fdisk_proc:
+    for fdisk_line in fdisk_proc.stdout:
+        fdisk_line = fdisk_line.rstrip()
+        fdisk_sek = re.search('^Disk /dev/(sd.*?): (.*?),', fdisk_line)
+        if fdisk_sek:
+            # basic information
+            disk = {}
+            disk['name'] = fdisk_sek.group(1)
+            disk['capacity'] = fdisk_sek.group(2)
 
-        for smart_line in smart_proc.stdout:
-            # check for all parameters
-            for param in smart_params:
-                smart_sek = re.search(param['re'], smart_line)
-                if smart_sek:
-                    disk['smart'][param['name']] = smart_sek.group(1)
+            # SMART
+            smart_proc = subprocess.Popen(['smartctl', '-a', '/dev/' + disk['name']], stdout=subprocess.PIPE)
+            disk['smart'] = {}
 
-        # save into data
-        data['disks'][disk['name']] = disk
+            for smart_line in smart_proc.stdout:
+                # check for all parameters
+                for param in smart_params:
+                    smart_sek = re.search(param['re'], smart_line)
+                    if smart_sek:
+                        disk['smart'][param['name']] = smart_sek.group(1)
+
+            # save into data
+            data['disks'][disk['name']] = disk
 
 # ipmitool
-ipmi_proc = subprocess.Popen(['ipmitool', 'fru'], stdout=subprocess.PIPE)
-for ipmi_line in ipmi_proc.stdout:
-    ipmi_line = ipmi_line.rstrip()
-    ipmi_sek = re.search('[\s]*(.*?)[\s]*:[\s]*(.*)[\s]*', ipmi_line)
-    if ipmi_sek:
-        data['ipmi'][ipmi_sek.group(1)] = ipmi_sek.group(2)
+try:
+    ipmi_proc = subprocess.Popen(['ipmitool', 'fru'], stdout=subprocess.PIPE)
+except OSError as e:
+    ipmi_proc = False
+    data['errors']['ipmi'] = str(e)
+
+if ipmi_proc:
+    for ipmi_line in ipmi_proc.stdout:
+        ipmi_line = ipmi_line.rstrip()
+        ipmi_sek = re.search('[\s]*(.*?)[\s]*:[\s]*(.*)[\s]*', ipmi_line)
+        if ipmi_sek:
+            data['ipmi'][ipmi_sek.group(1)] = ipmi_sek.group(2)
 
 # print output
 print(json.dumps(data))
